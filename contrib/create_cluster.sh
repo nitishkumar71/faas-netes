e67bb2a14bc5f67fe1a68f0bb9bd5c3ae397cec7
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2021-07-07 23:31:28 +0530

    Debug cluster

--- contrib/create_cluster.sh
+++ contrib/create_cluster.sh
@@ -5,7 +5,9 @@ KUBE_VERSION=v1.18.8
 
 echo ">>> Creating Kubernetes ${KUBE_VERSION} cluster ${DEVENV}"
 
-kind create cluster --wait 5m --image kindest/node:${KUBE_VERSION} --name "$DEVENV" -v 5
+kind create cluster --image kindest/node:${KUBE_VERSION} --name "$DEVENV" -v 5
 
+echo ">>> Get overall cluster health"
+kubectl --context "kind-$DEVENV" cluster-info dump
 echo ">>> Waiting for CoreDNS"
 kubectl --context "kind-$DEVENV" -n kube-system rollout status deployment/coredns
